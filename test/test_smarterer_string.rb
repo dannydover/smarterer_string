a5dd98cf7be6109705711fa5ff3bfd89362b0d37
Danny Dover <danny@intriguingideas.com>
2012-09-11 16:23:50 -0700

    refactored old tests

--- test/test_smarterer_string.rb
+++ test/test_smarterer_string.rb
@@ -2,8 +2,18 @@ require 'test/unit'
 require 'smarterer_string'
 
 class SmartererStringTest < Test::Unit::TestCase
-  def test_simple
-    assert_equal "hello world", "hello world"
+  def test_obfuscate
+    assert_equal "*** string of *** ***** is bigger", 
+      SmartererString.obfuscate("big string of big words is bigger", ["big", "words"])
+  end
+  
+  def test_report
+    h = Hash.new
+    h["big"] = 2
+    h["words"] = 1
+    
+    assert_equal h, 
+      SmartererString.report("big string of big words is bigger", ["big", "words"])
   end
 
 end

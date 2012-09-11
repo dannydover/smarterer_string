a40aa2ab89fee9a0e7b41a55d2cceb0db6050741
Danny Dover <danny@intriguingideas.com>
2012-09-11 16:32:38 -0700

    Added clearer documentation

--- lib/smarterer_string.rb
+++ lib/smarterer_string.rb
@@ -1,11 +1,24 @@
+# The main SmartererString driver
 class SmartererString
-
+  # Two string manipulation functions for Starter League code challenge
+  #
+  # Examples:
+  #   >> SmartererString.obfuscate("big string of big words is bigger", ["big", "words"])
+  #   => "*** string of *** ***** is bigger"
+  #
+  #   >> SmartererString.report("big string of big words is bigger", ["big", "words"])
+  #   => {"big"=>2, "words"=>1} 
+  #
+  # Arguments:
+  #   string: (String)
+  #   list: (array)  
+  
+  
   # This function takes a string and an array of words to be obfuscated.
   # It returns a modified version of the original string with the applicable
   # letters replaced with asterixis. The applicable words are those that are listed in the
   # provided list of words.
   # The regex in the gsub function matches any whole word that is passed to it as the var 'word'
-  
   def self.obfuscate(string, list)
   
     list.each do |word|
@@ -21,7 +34,6 @@ class SmartererString
   # in the given string.
   # This function takes the conservative route of splitting the string by anything 
   # non-alpha
-
   def self.report(string, list)
     
     words = string.split(/[^a-zA-Z]/)
